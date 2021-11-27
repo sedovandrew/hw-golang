3834b99e37b21966f49167a97f6f18e4118584fe
Andrey Sedov <sedoy80@gmail.com>
2021-11-27 16:34:16 +0300

    hw02: Added implementation of string unpacking and some tests

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,33 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(packString string) (string, error) {
+	var stringBuilder strings.Builder
+	var buffer string
+	for _, char := range packString {
+		if unicode.IsDigit(char) {
+			charRepeat, err := strconv.Atoi(string(char))
+			if err != nil {
+				return "", err
+			}
+			if buffer != "" {
+				stringBuilder.WriteString(strings.Repeat(buffer, charRepeat))
+				buffer = ""
+			} else {
+				return "", ErrInvalidString
+			}
+		} else {
+			stringBuilder.WriteString(buffer)
+			buffer = string(char)
+		}
+	}
+	stringBuilder.WriteString(buffer)
+	return stringBuilder.String(), nil
 }
