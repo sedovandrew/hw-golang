3182c2e8248d1194393c36dd84b6ecbd605e2c44
Andrey Sedov <sedoy80@gmail.com>
2022-01-15 13:50:36 +0300

    hw04: LRU cache

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,26 +1,84 @@
 package hw04lrucache
 
+import (
+	"sync"
+)
+
+// Key is the type of key in the cache.
 type Key string
 
+// Cache is a cache interface.
 type Cache interface {
 	Set(key Key, value interface{}) bool
 	Get(key Key) (interface{}, bool)
 	Clear()
 }
 
+// lruCache is a cache structure.
 type lruCache struct {
-	Cache // Remove me after realization.
-
+	mu       sync.Mutex
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
 }
 
+// Set adds an element to the cache.
+func (c *lruCache) Set(key Key, value interface{}) bool {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	// Already existing key
+	if listItem, ok := c.items[key]; ok {
+		listItem.Value = cacheItem{key: key, value: value}
+		c.queue.MoveToFront(listItem)
+		return true
+	}
+
+	// Drop last element
+	if c.queue.Len() >= c.capacity {
+		lastListItem := c.queue.Back()
+		c.queue.Remove(lastListItem)
+		delete(c.items, lastListItem.Value.(cacheItem).key)
+	}
+	// New key
+	listItem := c.queue.PushFront(
+		cacheItem{
+			key:   key,
+			value: value,
+		},
+	)
+	c.items[key] = listItem
+	return false
+}
+
+// Get returns an element from the cache.
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if listItem, ok := c.items[key]; ok {
+		c.queue.MoveToFront(listItem)
+		return listItem.Value.(cacheItem).value, true
+	}
+	return nil, false
+}
+
+// Clear clears the cache.
+func (c *lruCache) Clear() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.items = make(map[Key]*ListItem)
+	c.queue = NewList()
+}
+
+// cacheItem is a structure for storing an item in the cache.
 type cacheItem struct {
 	key   Key
 	value interface{}
 }
 
+// NewCache creates cache.
 func NewCache(capacity int) Cache {
 	return &lruCache{
 		capacity: capacity,
--- hw04_lru_cache/cache_test.go
+++ hw04_lru_cache/cache_test.go
@@ -50,13 +50,74 @@ func TestCache(t *testing.T) {
 	})
 
 	t.Run("purge logic", func(t *testing.T) {
-		// Write me
+		c := NewCache(5)
+		data := [...]Key{"zero", "one", "two", "three", "four"}
+		for index, item := range data {
+			c.Set(item, index)
+		}
+
+		c.Clear()
+
+		for _, item := range data {
+			value, ok := c.Get(item)
+			require.False(t, ok)
+			require.Nil(t, value)
+		}
+	})
+}
+
+func TestCacheOverflow(t *testing.T) {
+	t.Run("simple cache overflow", func(t *testing.T) {
+		c := NewCache(2)
+		c.Set("first", 1)
+		c.Set("second", 2)
+		c.Set("third", 3)
+
+		first, ok := c.Get("first")
+		require.False(t, ok)
+		require.Nil(t, first)
+	})
+
+	t.Run("complex cache overflow", func(t *testing.T) {
+		testData := map[string]string{
+			"one":   "first",
+			"two":   "second",
+			"three": "third",
+			"four":  "fourth",
+		}
+
+		// Creating a cache with test data.
+		c := NewCache(3)
+		c.Set("one", testData["one"])     // [one]
+		c.Set("two", testData["two"])     // [two, one]
+		c.Set("three", testData["three"]) // [three, two, one]
+		c.Get("one")                      // [one, three, two]
+		c.Get("three")                    // [three, one, two]
+		c.Set("four", testData["four"])   // [four, three, one]
+		c.Get("two")                      // [four, three, one]
+		c.Get("one")                      // [one, four, three]
+
+		// Checking existing elements
+		oneValue, ok := c.Get("one")
+		require.True(t, ok)
+		require.Equal(t, oneValue, testData["one"])
+
+		threeValue, ok := c.Get("three")
+		require.True(t, ok)
+		require.Equal(t, threeValue, testData["three"])
+
+		fourValue, ok := c.Get("four")
+		require.True(t, ok)
+		require.Equal(t, fourValue, testData["four"])
+
+		// Checking missing elements
+		twoValue, ok := c.Get("two")
+		require.False(t, ok)
+		require.Nil(t, twoValue)
 	})
 }
 
 func TestCacheMultithreading(t *testing.T) {
-	t.Skip() // Remove me if task with asterisk completed.
-
 	c := NewCache(10)
 	wg := &sync.WaitGroup{}
 	wg.Add(2)
